5ea4e94025027597973d2a51c5d053e4039d96fc
ZoreX <aydinhsimli@gmail.com>
2022-11-29 18:12:14 +0400

    done

--- WPFStoreUI/AddUc.xaml.cs
+++ WPFStoreUI/AddUc.xaml.cs
@@ -29,7 +29,7 @@ namespace WPFStoreUI
         {
             InitializeComponent();
             this.DataContext = this;
-            Product = product;
+            this.Product = product;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
